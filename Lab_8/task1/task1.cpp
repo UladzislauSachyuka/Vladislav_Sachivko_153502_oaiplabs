3285cf461fe35204864e1e837f333ab75e7ee551
Влад Сачивко <VladSachivko@yandex.ru>
2021-12-27 18:09:46 +0300

    added laba 8

---
+++ Lab_8/task1/task1.cpp
@@ -0,0 +1,435 @@
+﻿// Лабораторная 8, задача 1. Выполнил Сачивко В.Г.
+/* Различные цеха завода выпускают продукцию нескольких наименований.
+Сведения о выпущенной продукции включают наименование, количество, номер
+цеха. Для заданного цеха вывести количество выпущенных изделий (Шейкер
+сортировка по убыванию).*/
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+struct Workshop 
+{
+	string name;
+	int quantity;
+	int workshop_number;
+	int price;
+	int exportation;
+};
+
+int getinput()
+{
+	int input;
+	cin >> input;
+	while (cin.get() != '\n')
+	{
+		cin.clear();
+		cin.ignore(100000, '\n');
+		printf("Incorrect input. Try again.\nEnter the value\n");
+		cin >> input;
+	}
+	return input;
+}
+
+void seemenu() 
+{
+	printf("1.Entering a dynamic array of structures\n");
+	printf("2.Viewing the contents of a dynamic array of structures\n");
+	printf("3.Addition of new structures\n");
+	printf("4.Finding and displaying a structure with a given element value\n");
+	printf("5.Deleting a structure\n");
+	printf("6.Changing an array element\n");
+	printf("7.Sorting an array of structures by the number of products produced\n");
+	printf("8.Menu exit\n");
+}
+
+Workshop* AddStruct(Workshop* Obj, const int amount) 
+{
+	if (amount == 0) 
+	{
+		Obj = new Workshop[amount + 1];
+	}
+	else
+	{
+		Workshop* tempObj = new Workshop[amount + 1];
+
+		for (int i = 0; i < amount; ++i)
+		{
+			tempObj[i] = Obj[i];
+		}
+		delete[] Obj;
+
+		Obj = tempObj;
+	}
+	return Obj;
+}
+
+void setData(Workshop* Obj, const int amount) 
+{
+	printf("enter the product name\n");
+	char* buf = new char[100];
+	scanf("%s", buf);
+	Obj[amount].name = buf;
+	delete[] buf;
+	printf("enter the quantity of products produced\n");
+	Obj[amount].quantity = getinput();
+	printf("enter the workshop number\n");
+	Obj[amount].workshop_number = getinput();
+	printf("what is the price per product?\n");
+	Obj[amount].price = getinput();
+	printf("where will the products go (1 - for export, 0 - for the domestic market)?\n");
+	while (true)
+	{
+		Obj[amount].exportation = getinput();
+		if (Obj[amount].exportation == 0 || Obj[amount].exportation == 1)
+			break;
+		else
+			printf("Incorrect input\n");
+	}
+}
+
+void showData(const Workshop* Obj, const int amount) 
+{
+	printf("\n\nname\tquantity\tworkshop number\tprice per product\texport\n\n");
+	for (int i = 0; i < amount; ++i) 
+	{
+		printf(Obj[i].name.c_str());
+		printf("\t%d\t\t%d\t\t\t%d\t\t%d\n", Obj[i].quantity, Obj[i].workshop_number, Obj[i].price, Obj[i].exportation);
+	}
+}
+
+void shaker_sort(Workshop* Obj, const int amount) 
+{
+	int left = 0, right = amount - 1;   bool swapped = true;
+	while ((left < right) && swapped) 
+	{
+		swapped = false;
+		for (int i = left; i < right; i++) 
+		{
+			if (Obj[i].quantity < Obj[i + 1].quantity) 
+			{
+				Workshop temp = Obj[i]; Obj[i] = Obj[i + 1]; Obj[i + 1] = temp;
+				swapped = true;
+			}
+		}
+		right--;
+
+		for (int i = right; i > left; i--) 
+		{
+			if (Obj[i - 1].quantity < Obj[i].quantity) 
+			{
+				Workshop temp = Obj[i]; Obj[i] = Obj[i - 1]; Obj[i - 1] = temp;
+				swapped = true;
+			}
+		}
+		left++;
+	}
+}
+
+Workshop* Delete(Workshop* Obj, int& amount, int num) 
+{
+	Workshop* tempObj = new Workshop[amount - 1];
+
+	for (int i = 0, j = 0; i < amount; ++i, ++j)
+	{
+		if (i != num)
+			tempObj[j] = Obj[i];
+		else
+			--j;
+	}
+	--amount;
+
+	delete[] Obj;
+
+	Obj = tempObj;
+
+	return Obj;
+}
+
+void find(Workshop* Obj, const int amount) 
+{
+	printf("choose a parameter\n1.Name 2.Quantity 3.Workshop number 4.Price 5.Exportation\n");
+	int parameter, quant, price, num, exp;
+	string name1;
+	parameter = getinput();
+
+	switch (parameter) 
+	{
+	case 1:
+		printf("enter the name\n");
+		cin >> name1;
+		for (int i = 0; i < amount; ++i) 
+		{
+			if (Obj[i].name == name1)
+			{
+				printf(Obj[i].name.c_str());
+				printf("\t%d\t\t%d\t\t\t%d\t\t%d\n", Obj[i].quantity, Obj[i].workshop_number, Obj[i].price, Obj[i].exportation);
+			}
+		}
+		break;
+	case 2:
+		printf("enter the quantity\n");
+		quant = getinput();
+		for (int i = 0; i < amount; ++i)
+		{
+			if (Obj[i].quantity == quant)
+			{
+				printf(Obj[i].name.c_str());
+				printf("\t%d\t\t%d\t\t\t%d\t\t%d\n", Obj[i].quantity, Obj[i].workshop_number, Obj[i].price, Obj[i].exportation);
+			}
+		}
+		break;
+	case 3:
+		printf("enter the workshop number\n");
+		num = getinput();
+		for (int i = 0; i < amount; ++i)
+		{
+			if (Obj[i].workshop_number == num)
+			{
+				printf(Obj[i].name.c_str());
+				printf("\t%d\t\t%d\t\t\t%d\t\t%d\n", Obj[i].quantity, Obj[i].workshop_number, Obj[i].price, Obj[i].exportation);
+			}
+		}
+		break;
+	case 4:
+		printf("enter the price\n");
+		price = getinput();
+		for (int i = 0; i < amount; ++i)
+		{
+			if (Obj[i].price == price)
+			{
+				printf(Obj[i].name.c_str());
+				printf("\t%d\t\t%d\t\t\t%d\t\t%d\n", Obj[i].quantity, Obj[i].workshop_number, Obj[i].price, Obj[i].exportation);
+			}
+		}
+		break;
+	case 5:
+		printf("enter exportation(1 - for export, 0 - for the domestic market)\n");
+		while (true)
+		{
+			exp = getinput();
+			if (exp == 0 || exp == 1)
+				break;
+			else
+				printf("Incorrect input\n");
+		}
+		for (int i = 0; i < amount; ++i)
+		{
+			if (Obj[i].exportation == exp)
+			{
+				printf(Obj[i].name.c_str());
+				printf("\t%d\t\t%d\t\t\t%d\t\t%d\n", Obj[i].quantity, Obj[i].workshop_number, Obj[i].price, Obj[i].exportation);
+			}
+		}
+		break;
+	default:
+		printf("there is no such parameter\n");
+		break;
+	}
+}
+
+Workshop* input(Workshop* Workshops, int& amount)
+{
+	int n;
+	int YesOrNot = 0;
+	printf("Enter 1, if you want a certain number of structures\n");
+	printf("Enter 2, if you want input before the structure with the specified attribute appears\n");
+	printf("Enter 3, if you want to have a dialog\n");
+	while (true) 
+	{
+		n = getinput();
+		if (n == 1 || n == 2 || n == 3)
+			break;
+		else
+			printf("Incorrect input\n");
+	}
+	
+	switch (n)
+	{
+	case 1:
+		printf("enter the number of structures\n");
+		amount = getinput();
+		Workshops = new Workshop[amount];
+		for (int i = 0; i < amount; ++i)
+		{
+			printf("enter the product name\n");
+			char* buf = new char[100];
+			scanf("%s", buf);
+			Workshops[i].name = buf;
+			delete[] buf;
+			printf("enter the quantity of products produced\n");
+			Workshops[i].quantity = getinput();
+			printf("enter the workshop number\n");
+			Workshops[i].workshop_number = getinput();
+			printf("what is the price per product?\n");
+			Workshops[i].price = getinput();
+			printf("where will the products go (1 - for export, 0 - for the domestic market)?\n");
+			while (true)
+			{
+				Workshops[i].exportation = getinput();
+				if (Workshops[i].exportation == 0 || Workshops[i].exportation == 1)
+					break;
+				else
+					printf("Incorrect input\n");
+			}
+		}
+		break;
+	case 2:
+		do
+		{
+			Workshops = AddStruct(Workshops, amount);
+			setData(Workshops, amount);
+			++amount;
+		} while (Workshops[amount - 1].name != "0" && Workshops[amount - 1].quantity != 0 && Workshops[amount - 1].workshop_number != 0
+			&& Workshops[amount - 1].price != 0 && Workshops[amount - 1].exportation != 0);
+		break;
+	case 3:
+		do
+		{
+			Workshops = AddStruct(Workshops, amount);
+			setData(Workshops, amount);
+
+			++amount;
+
+			printf("do you want to continue(1 - yes, 0 - no)?\n");
+			while (true)
+			{
+				YesOrNot = getinput();
+				if (YesOrNot == 0 || YesOrNot == 1)
+					break;
+				else
+					printf("Incorrect input\n");
+			}
+		} while (YesOrNot != 0);
+		break;
+	default: 
+		printf("try again\n");
+		break;
+	}
+	return Workshops;
+}
+
+int main() 
+{
+	Workshop* Workshops = 0;
+	Workshop* NewWorkshops;
+	int workshop_amount = 0;
+	int YesOrNot = 0;
+	int menu, k = 0, num;
+	bool fl = false;
+	seemenu();
+	menu = getinput();
+	while (true)
+	{
+		switch (menu)
+		{
+		case 1:
+			Workshops = input(Workshops, workshop_amount);
+			break;
+		case 2:
+			if (Workshops != nullptr)
+				showData(Workshops, workshop_amount);
+			else
+				printf("there is no data\n");
+			break;
+		case 3:
+			Workshops = AddStruct(Workshops, workshop_amount);
+			++workshop_amount;
+			setData(Workshops, workshop_amount - 1);
+			break;
+		case  4:
+			if (Workshops != nullptr) 
+				find(Workshops, workshop_amount);
+			else 
+				printf("there is no data\n");
+			break;
+		case 5:
+			if (Workshops != nullptr)
+			{
+				printf("what structure do you want to delete?\n");
+				while (true)
+				{
+					num = getinput();
+					if (num > 0 && num <= workshop_amount)
+						break;
+					else
+						printf("Incorrect input\n");
+				}
+				Workshops = Delete(Workshops, workshop_amount, num - 1);
+			}
+			else 
+				printf("there is no data\n");
+			break;
+		case 6:
+			if (Workshops != nullptr)
+			{
+				printf("what structure do you want to change?\n");
+				while (true)
+				{
+					num = getinput();
+					if (num > 0 && num <= workshop_amount)
+						break;
+					else
+						printf("Incorrect input\n");
+				}
+				setData(Workshops, num - 1);
+			}
+			else 
+				printf("there is no data\n");
+			break;
+		case 7:
+			if (Workshops != nullptr)
+			{
+				shaker_sort(Workshops, workshop_amount);
+				showData(Workshops, workshop_amount);
+			}
+			else 
+				printf("there is no data\n");
+			break;
+		case 8:
+			if (Workshops != nullptr)
+			{
+				fl = true;
+				break;
+			}
+			else 
+				printf("there is no data\n");
+		default:
+			exit(0);
+			break;
+		}
+		if (fl == true)
+			break;
+		seemenu();
+		menu = getinput();
+	}
+
+	printf("enter the number of the workshop for which you want to find out the quantity of products\n");
+	while (true)
+	{
+		num = getinput();
+		if (num > 0 && num <= workshop_amount)
+			break;
+		else
+			printf("Incorrect input\n");
+	}
+	bool flag = false;
+	for (int i = 0; i < workshop_amount; ++i) 
+	{
+		if (Workshops[i].workshop_number == num) 
+		{
+			printf("the quantity of products is ");
+			printf("%d\n", Workshops[i].quantity);
+			flag = true;
+		}
+	}
+
+	if (flag == false)
+		printf("there is no such workshop\n");
+
+	delete[] Workshops;
+
+	return 0;
+}
---
+++ Lab_8/task3/task3.cpp
@@ -0,0 +1,347 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+
+string P_in = "inTask3.txt";
+string P_out = "outTask3.txt";
+ofstream fout;
+ifstream fin;
+
+int getinput()
+{
+	int input;
+	cin >> input;
+	while (cin.get() != '\n')
+	{
+		cin.clear();
+		cin.ignore(100000, '\n');
+		cout << "Incorrect input. Try again.\nEnter the value\n";
+		cin >> input;
+	}
+	return input;
+}
+
+class Employee 
+{
+public:
+
+    Employee() {
+        char name[] = " ";
+        char last_name[] = " ";
+        char patronymic[] = " ";
+        char pers_number[] = " ";
+        char hours_worked[] = " ";
+        char hourly_rate[] = " ";
+    }
+
+    void print_employee() 
+    {
+        cout << "Name: " << name << '\n';
+        cout << "Last name:" << last_name << '\n';
+        cout << "Patronymic: " << patronymic << '\n';
+        cout << "Personal number: " << pers_number << '\n';
+        cout << "Working hours: " << hours_worked << '\n';
+        cout << "Hourly rate: " << hourly_rate << '\n';
+    }
+
+    void set_inf() 
+    {
+        int num;
+        double hours;
+        double h_rate;
+        cout << "Type the name of worker\n";
+        cin >> name;
+        cout << "Type the last name of worker\n";
+        cin >> last_name;
+        cout << "Type the patronymic of worker\n";
+        cin >> patronymic;
+        cout << "Type the personal number of worker\n";
+        num = getinput();
+        _itoa(num, pers_number, 10);
+        cout << "Type the hours person worked\n";
+        hours = getinput();
+        _itoa(hours, hours_worked, 10);
+        cout << "Type the hourly rate\n";
+        h_rate = getinput();
+        _itoa(h_rate, hourly_rate, 10);
+    }
+
+    double get_salary() 
+    {
+        double overtime = 0;
+        if (atoi(hours_worked) > 144) 
+        {
+            overtime = atoi(hours_worked) - 144;
+            _itoa(144, hours_worked, 10);
+        }
+        double salary = (atoi(hours_worked) * atoi(hourly_rate) + overtime * 2 * atoi(hourly_rate)) * 0.88;
+        return salary;
+    }
+
+    void print_salary() 
+    {
+        cout << "Worker " << name << " " << last_name << " " << patronymic << " get salary\n";
+        cout << get_salary();
+        cout << "\n\n";
+    }
+
+private:
+    char name[15];
+    char last_name[15];
+    char patronymic[15];
+    char pers_number[15];
+    char hours_worked[15];
+    char hourly_rate[15];
+};
+
+void input(int* n) 
+{
+    cout << "Enter the number of workers\n";
+    int k;
+    k = getinput();
+
+    Employee* em = (Employee*)calloc(k, sizeof(Employee));
+
+    fout.open(P_out, ofstream::app & ofstream::trunc);
+
+    if (!fout.is_open()) 
+        cout << "Cannot open the file!\n";
+    else 
+    {
+        for (int i = 0; i < k; ++i) 
+        {
+            em[i].set_inf();
+            fout.write((char*)&em[i], sizeof(Employee));
+        }
+    }
+    fout.close();
+    *n = k;
+}
+
+void view(int size) 
+{
+    fin.open(P_out);
+    auto e = (Employee*)calloc(size, sizeof(Employee));
+    if (!fin.is_open()) 
+        cout << "Cannot open the file!\n";
+    else 
+    {
+        for (int i = 0; i < size; i++) 
+        {
+            fin.read((char*)&e[i], sizeof(Employee));
+            cout << i + 1 << ":\n";
+            e[i].print_employee();
+            cout << "\n\n";
+        }
+    }
+    fin.close();
+}
+
+void add(int* n) 
+{
+    fout.open(P_out, fstream::app);
+    if (!fout.is_open()) 
+        cout << "Cannot open the file!\n";
+    else 
+    {
+        int size = (*n);
+        cout << "Enter the number of records you want to add\n";
+        int rec;
+        rec = getinput();
+        auto* e = (Employee*)calloc(rec, sizeof(Employee));
+        for (int i = 0; i < rec; ++i) 
+        {
+            e[i].set_inf();
+            fout.write((char*)&e[i], sizeof(Employee));
+        }
+        *n = size + rec;
+    }
+    fout.close();
+}
+
+void deleting(int* n) 
+{
+    cout << "Type the number of record you want to delete:\n";
+    int rec;
+    rec = getinput();
+    while (rec > (*n))
+    {       
+        cout << "Incorrect input\n";
+        rec = getinput();
+    }
+
+    fin.open(P_out);
+
+    for (int i = 0; i < rec; i++) 
+    {
+        fin.seekg(sizeof(Employee));
+    }
+
+    int size = 0;
+
+    for (int i = rec; i <= *(n); i++) 
+        size++;
+
+    Employee* employees = new Employee[size];
+
+    for (int i = 0; i < size; i++) 
+    {
+        fin.read((char*)&employees[i], sizeof(Employee));
+    }
+    fin.close();
+    for (int i = 0; i < size - 1; i++) 
+    {
+        employees[i] = employees[i + 1];
+    }
+    filesystem::resize_file(P_out, (rec - 1) * sizeof(Employee));
+
+    fout.open(P_out, ios_base::app);
+    for (int i = 0; i < size - 1; i++) 
+    {
+        fout.write((char*)&employees[i], sizeof(Employee));
+    }
+    fin.close();
+    (*n)--;
+}
+
+void edit(int n) 
+{
+    Employee* e = (Employee*)calloc(n, sizeof(Employee));
+    fin.open(P_out);
+    if (!fin.is_open()) 
+        cout << "Cannot open the file!\n";
+    else 
+    {
+        for (int i = 0; i < n; ++i) 
+        {
+            fin.read((char*)&e[i], sizeof(Employee));
+        }
+    }
+    fin.close();
+
+    cout << "Enter the number of worker you want to edit\n";
+    int rec;
+    rec = getinput();
+
+    while (rec < 0 || rec > n) 
+    {
+        cout << "Incorrect value\n";
+        cin.clear();
+        cin.ignore(32767, '/n');
+        rec = getinput();
+    }
+
+    for (int i = 0; i < n; ++i) 
+    {
+        if (i == rec - 1) 
+            e[i].set_inf();
+    }
+
+    fout.open(P_out, fstream::trunc);
+    if (!fout.is_open()) 
+        std::cout << "Cannot open the file!\n";
+    else 
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            fout.write((char*)&e[i], sizeof(Employee));
+        }
+    }
+    fout.close();
+}
+
+void task(int n) 
+{
+    Employee* e = (Employee*)calloc(n, sizeof(Employee));
+    fin.open(P_out);
+    if (!fin.is_open()) 
+        cout << "Cannot open the file!\n";
+    else 
+    {
+        for (int i = 0; i < n; ++i) 
+        {
+            fin.read((char*)&e[i], sizeof(Employee));
+        }
+    }
+    fin.close();
+
+    for (int i = 0; i < n; ++i) 
+    {
+        e[i].print_salary();
+    }
+
+    fout.open(P_in, std::fstream::app & std::fstream::trunc);
+    if (!fout.is_open()) 
+        std::cout << "Cannot open the file!\n";
+    else 
+    {
+        for (int i = 0; i < n; ++i) 
+        {
+            fout << e[i].get_salary() << "\n";
+        }
+    }
+    fout.close();
+}
+
+void show_menu() 
+{
+    cout << "1. Create new file\n";
+    cout << "2. Read the file\n";
+    cout << "3. Add new employee record\n";
+    cout << "4. Delete record\n";
+    cout << "5. Edit record\n";
+    cout << "6. Show employees salary\n";
+    cout << "7. Program exit\n";
+}
+
+int main() 
+{
+    int menu;
+    int n = 0;
+    bool is_exit = false;
+
+    while (!is_exit) 
+    {
+        show_menu();
+        menu = getinput();
+        switch (menu) 
+        {
+        case 1:
+            input(&n);
+            break;
+        case 2:
+            if (n == 0) 
+                cout << "You should add information before view it\n";
+            else 
+                view(n);
+            break;
+        case 3:
+            add(&n);
+            break;
+        case 4:
+            if (n == 0)
+                cout << "You should add information before deleting it\n";
+            else
+                deleting(&n);
+            break;
+        case 5:
+            if (n == 0) 
+                cout << "You should add information before changing it\n";
+            else 
+                edit(n);
+            break;
+        case 6:
+            if (n == 0) 
+                cout << "You should add information before completing the task\n";
+            else 
+                task(n);
+            break;
+        default:
+            is_exit = true;
+        }
+    }
+    return 0;
+}
