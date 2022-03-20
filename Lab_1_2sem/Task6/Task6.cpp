91051af9f30f5922fbcd8f66f724b6893faf1be2
UladzislauSachyuka <94551428+UladzislauSachyuka@users.noreply.github.com>
2022-03-20 12:00:44 +0300

    Delete Lab_1_2sem directory

--- Lab_1_2sem/Task6/Task6.cpp
+++
@@ -1,7 +0,0 @@
-﻿bool check_equals(Expression const* left, Expression const* right)
-{
-    int* vptr1 = (int*)left;
-    int* vptr2 = (int*)right;
-    if (*vptr1 == *vptr2) return true;
-    return false;
-}
